12d7f691d8d6c21fe5f03d47d5d1dd8205d70fb4
Zac Swider <zac@concertobio.com>
2025-03-26 19:17:57 -0400

    install rust analyzer

--- init.lua
+++ init.lua
@@ -690,7 +690,7 @@ require('lazy').setup({
         --     }
         --   }
         -- },
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
